595661885629bd5c711b96e016ec00d7d17f99b3
Kim Sangyeon <dho113@naver.com>
2018-09-25 22:54:53 +0900

    typescript_example/18.09.25/ callback

--- src/ts/callback/callback.ts
+++ src/ts/callback/callback.ts
@@ -1,3 +1,23 @@
-/**
- * Created by kimsangyeon on 2018. 9. 24..
- */
+
+function doCallback(data: string, callback: (datas: string[], length: number) => void): void {
+
+    let datas: string[] = [data, "2"];
+    callback(datas, datas.length);
+}
+
+function callback(datas: string[], length: number) {
+   
+}
+
+doCallback("sang", callback);
+
+
+interface ICallBack {
+    (datas: string[], length: number): void;
+}
+
+function doCallback(data: string, callback: ICallBack): void {
+
+    let datas: string[] = [data, "2"];
+    callback(datas, datas.length);
+}
